d19f4fec9eaa77dbd1a46cefbc67cfe3dc5b0458
Edward <cololacalempira5@gmail.com>
2026-08-09 13:55:42 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+export const config = {
+  botName: "SlowedGenX",
+  version: "1.0.0",
+  creator: "Duan & BrayanRK",
+  sessionFolder: "./sessions/main",
+  subBotsFolder: "./sessions/subbots",
+  ownerNumber: "50400000000",
+  canal: null,
+
+  welcome: {
+    mensajeBienvenida: "Llegaste, {mencion}. Bienvenida/o a {grupo}. Ya somos {cantidad}.",
+    mensajeDespedida: "{mencion} se fue de {grupo}. Ya somos {cantidad}.",
+  },
+};
